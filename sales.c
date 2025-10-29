42971b502d353b6052c32d10a692b29b32266a06
gwenrl <gwenrl58@gmail.com>
2025-10-29 17:01:43 -0400

    sales started

--- sales.c
+++ sales.c
@@ -1,11 +1,130 @@
-{
-Set up shared memory & initialize its objects ;
-Set up the semaphores & the message queue ;
-Fork / Execute Supervisor process ;
-Fork / Execute all Factory processes ;
-Wait for supervisor to indicate that manufacturing is done
-Grant supervisor permission to print the Final Report ;
-Clean up after zombie processes (Supervisor + all Factories) ;
-Destroy the shared memory ;
-Destroy the semaphores & the message queue ;
+// {
+// Set up shared memory & initialize its objects ;
+// Set up the semaphores & the message queue ;
+// Fork / Execute Supervisor process ;
+// Fork / Execute all Factory processes ;
+// Wait for supervisor to indicate that manufacturing is done
+// Grant supervisor permission to print the Final Report ;
+// Clean up after zombie processes (Supervisor + all Factories) ;
+// Destroy the shared memory ;
+// Destroy the semaphores & the message queue ;
+// }
+
+shmData    *p;
+int        shmid ;
+
+pid_t supervisor_pid;
+pid_t *factory_pids;
+//shared memory vars
+key_t      shmkey;
+int        shmflg;
+//named semaphore vars
+int        semMode;
+int        semflg;
+//message queue vars
+key_t msgkey;
+int msgqid;
+int msgflags;
+
+
+void clean_up() {
+    //send KILL signal to all child processes
+    kill(supervisor_pid, SIGKILL);
+    for (int i = 0; i < p->num_factories; i++) {
+        kill(factory_pids[i], SIGKILL);
+    }
+    //clean up semaphores
+
+    //remove the shared memory and destroy message queue
+    Shmdt(p);
+    shclt(shmid, IPC_RMID, NULL);
+
+    msgctl(msgqid, IPC_RMID, NULL);
+    
+    //wait
+    waitpid(supervisor_pid, &status, 0);
+    for (int i = 0; i < num_factories; i++) {
+        waitpid(factory_pids[i], &status, 0);
+    }
+}
+
+
+int main( int argc , char *argv[] ) {
+
+    //set up the shared memory & initialize its objects
+    shmkey = ftok("shmem.h", 5) ;
+    shmflg = IPC_CREAT | IPC_EXCL  | S_IRUSR | S_IWUSR ;
+
+    shmid = Shmget(shmkey, SHMEM_SIZE, shmflg) ;
+
+    p = Shmat(shmid, NULL, 0) ; 
+
+    int num_factories = atoi(argv[1]);
+
+    if (num_factories > MAXFACTORIES) {
+        num_factories = MAXFACTORIES;
+    }
+
+    int order_size = atoi(argv[2]);
+
+
+    p->order_size = order_size;    p->made = 0;    p->remain = order_size;
+
+    //set up the semaphores & the message queue
+    //idk what semaphores yet :(
+
+    //message queue set up?
+
+    msgflgs = IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR | S_IWOTH | S_IWGRP | S_IRGRP | S_IROTH ;
+    msgkey = ftok(".", 6);
+
+    msgqid = Msgget(msgkey, msgflgs);
+
+    // Fork / Execute Supervisor process ;
+    
+    supervisor_pid = Fork()
+
+    if (supervisor_pid == 0) {
+        //redirect stdout
+        FILE *supervisor_log = fopen("supervisor.log", "w");
+        dup2(stdout, "supervisor.log");
+
+        //execute
+        execlp("./Supervisor", num_factories , NULL);
+    }
+
+    for (int i = 0; i < num_factories; i++) {
+        pid_t factory_pid = Fork();
+
+        if (factory_pid == 0){
+            //redirect stdout
+            FILE *factory_log = fopen("factory.log", "w");
+            dup2(stdout, "factory.log");
+
+            srandom(time(NULL));
+
+            int factory_ID = i;
+            int capacity = (random() % (50 - 10 + 1) + 10);
+            int duration = (random() % (1200 - 500 + 1) + 500);
+
+            execlp("./Factory", factory_ID, capacity, duration, NULL)
+        }
+
+        factory_pids[i] = factory_pid;
+    }
+
+    //handle abnormal termination
+    sigactionWrapper(SIGINT, clean_up());
+    sigactionWrapper(SIGTERM, clean_up());
+
+    //wait for supervisor via rendezvous semaphore
+
+    //simulate printer by sleeping
+    Usleep(2);
+
+    //give permission using rendezvous semaphore to the supervisor to print
+
+    //wait for children/clean up
+    clean_up();
+    
 }
